82a546c2d207790ef06b1c414d1597cb2cde19a0
Reese Stowell <Reese2as@gmail.com>
2021-09-05 20:42:11 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,16 @@
+/*
+UCF COP3330 Fall 2021 Exercise 1 Solution
+Copyright 2021 Reese Stowell
+*/
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args)
+{
+ Scanner user_input = new Scanner(System.in);
+ String name;
+ System.out.print("What is your name?");
+ name = user_input.next();
+ System.out.println(String.format("Hello, %s, nice to meet you!",name));
+}
+}
